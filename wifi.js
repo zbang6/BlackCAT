ed13d2b767f459b984cbad93a34b7fdedd753dea
zbang6 <66144920+zbang6@users.noreply.github.com>
2020-07-05 21:55:51 +0800

    Update wifi.js

--- wifi.js
+++ wifi.js
@@ -1,16 +1,23 @@
+
 const SSID = $network.wifi.ssid;
-const proxywifi = ["zhang_5G","zhang"];
+const proxywifi = !$persistentStore.read("lkWifiSsids")?["zhang_5G","zhang"]:JSON.parse($persistentStore.read("lkWifiSsids"));
 let res = proxywifi.some(val => val === SSID);
-if (res) {
-$surge.setOutboundMode("direct");
-notify(1);
-} else {
-$surge.setOutboundMode("rule");
-notify(0);
+let lkWifiLast = !$persistentStore.read("lkWifiLast")?"abcdefghijklmnopqrstuvwxyz":$persistentStore.read("lkWifiLast");
+if (lkWifiLast!=(!!SSID ? SSID : "cellular")){
+    !$persistentStore.write((!!SSID ? SSID : "cellular"), "lkWifiLast")
+    if (res) {
+        $surge.setOutboundMode("direct");
+        notify(1);
+    } else {
+        $surge.setOutboundMode("rule");
+        notify(0);
+    }
 }
 
 function notify(mode) {
-setTimeout(function(){
-!!mode ? $notification.post("Wi-Fi助手","切换直连模式",`正在使用Wi-Fi:${SSID}`) : $notification.post("Wi-Fi助手","切换规则模式", `${!!SSID ?  "your SSID is " + SSID :"正在使用蜂窝网络"}`)
-}) }
+    setTimeout(function () {
+        !!mode ? $notification.post("Wi-Fi助手", "切换直连模式", `your SSID is ${SSID}`) : $notification.post("Wi-Fi助手", "切换规则模式", `${!!SSID ? "your SSID is " + SSID : "正在使用蜂窝网络"}`)
+    })
+}
+
 $done();
