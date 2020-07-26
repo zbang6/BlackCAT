4b5c208f54da62dcb5fa01bd2f78d1dc46a34368
zbang6 <66144920+zbang6@users.noreply.github.com>
2020-07-26 14:57:05 +0800

    Create zhihu.js

---
+++ zhihu.js
@@ -0,0 +1,27 @@
+#知乎
+^https://www\.zhihu\.com/question/ url script-response-body id77/Script/zhihu.js
+
+#脚本
+#=====
+let html = $response.body
+let nonce= html.match(/nonce="[\w\-]*"/g)[1]
+
+html = html.replace(/(<\/html>)/g, "") +
+`
+<script ${nonce}>
+setTimeout(
+() => {
+document.querySelector("body").style.overflow = "auto"    document.querySelector(".MobileModal-wrapper").remove()
+
+    document.querySelectorAll(".RichContent-inner").forEach(item => {
+        item.style.maxHeight = "100%"
+        item.parentNode.removeAttribute("class")
+    })
+}
+,
+600
+)
+</script>
+</html>
+`
+$done({body: html})
