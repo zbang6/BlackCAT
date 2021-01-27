5f64551f30a63c9515f608e699e7b40a7397feb7
BlackCAT <zbang6@users.noreply.github.com>
2021-01-27 19:28:57 +0800

    Update

--- QuantumultX/Js/IP-QX.js
+++ QuantumultX/Js/IP-QX.js
@@ -4,7 +4,7 @@ if ($response.statusCode != 200) {
 
 const emojis= ['🆘','🈲','⚠️','🔞','📵','🚦','🏖','🖥','📺','🐧','🐬','🦉','🍄','⛳️','🚴','🤑','👽','🤖','🎃', '👺', '👁', '🐶', '🐼','🐌', '👥']
 var city0 = "ʜᴇʟʟᴏ";
-var isp0 = "QX天下第一!";
+var isp0 = "QX天下第一 !";
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
