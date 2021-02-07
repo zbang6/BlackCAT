c6f1825ac1e5bd4c899c12f633590ce506d72919
BlackCAT <zbang6@users.noreply.github.com>
2021-02-07 09:09:56 +0800

    Update

--- QuantumultX/Js/IP-QX.js
+++ QuantumultX/Js/IP-QX.js
@@ -41,7 +41,7 @@ var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "�
 var body = $response.body;
 var obj = JSON.parse(body);
 var title =City_ValidCheck(obj['city0']);//+Area_check(obj['country']);
-var subtitle = '🗣 '+ISP_ValidCheck(obj['isp']);
+var subtitle = ISP_ValidCheck(obj['isp']);
 var ip = obj['query'];
 var description = '服务商:'+obj['isp'] + '\n'+'地区:' +City_ValidCheck(obj['regionName'])+ '\n' + 'IP:'+ obj['query'] + '\n' +'时区:'+ obj['timezone'];
 $done({title, subtitle, ip, description});
