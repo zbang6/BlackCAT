4cb300692570a6d28bd61be0ba11088e140504e0
BlackCAT <zbang6@users.noreply.github.com>
2021-11-28 16:07:15 +0800

    Update

--- Surge/Js/wifi_proxy.js
+++ Surge/Js/wifi_proxy.js
@@ -1,12 +1,10 @@
-/* wifi_proxy change (Made by Meeta)
-文本编辑模式下复制粘贴,TG频道已发操作教程视频
+
 event network-changed debug=1,script-path=wifi_proxy.js
 PS:记得自己修改WIFI名称
 虽然设置SSID可以达到基本相同功能
 使用脚本,Surge不会被suspend
 Rewrite和Scripting依然有效
-https://meetagit.github.io/MeetaRules/Surge/Scripting/wifi_proxy.js
-TG频道:@meetashare
+
 */
 
 var wifiname = $network.wifi.ssid;
